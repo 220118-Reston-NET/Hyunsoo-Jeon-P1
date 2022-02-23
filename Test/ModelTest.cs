29b62a7cffc3dc070b2de0d7b1170da55831bddc
Hyunsoo Jeon <hsprime85@gmail.com>
2022-02-23 13:58:07 -0800

    Added model Test

--- Test/ModelTest.cs
+++ Test/ModelTest.cs
@@ -53,6 +53,93 @@ namespace Test
             Assert.Equal(validName, name.Name);
 
         }
+
+        [Fact]
+        public void CustomerAddressShouldValidData()
+        {
+            //arrange
+            Customer address = new Customer();
+            string validAddress = "1234 main st, san jose, ca 99999";
+
+            //act
+            address.Address = validAddress;
+
+            // assert
+            Assert.NotNull(address.Address);
+            Assert.Equal(validAddress, address.Address);
+
+        }
     }
-    
+
+    public class InventoryModelTest
+    {
+        [Fact]
+        public void InventoryStoreIDShouldValidData()
+        {
+            Inventory storeId = new Inventory();
+            int validId = 1;
+
+            storeId.StoreID = validId;
+
+            Assert.NotNull(storeId.StoreID);
+            Assert.Equal(validId, storeId.StoreID);
+        }
+
+        [Fact]
+        public void InventoryQuantityShouldValidData()
+        {
+            Inventory qty = new Inventory();
+            int validQty = 40;
+
+            qty.Qty = validQty;
+
+            Assert.NotNull(qty.Qty);
+            Assert.Equal(validQty, qty.Qty);
+        }
+    }
+
+
+
+
+    public class LineItemModelTest
+    {
+        [Fact]
+        public void LineItemProductNameShouldValidData()
+        {
+            LineItems productName = new LineItems();
+            string validName = "brake fluid";
+
+            productName.ProductName = validName;
+
+            Assert.NotNull(productName.ProductName);
+            Assert.Equal(validName, productName.ProductName);
+        }
+        
+        [Fact]
+        public void LineItemQuantityShouldValidData()
+        {
+            LineItems qty = new LineItems();
+            int validQty = 3;
+
+            qty.Qty = validQty;
+
+            Assert.NotNull(qty.Qty);
+            Assert.Equal(validQty, qty.Qty);
+        }
+    }
+
+    public class StoreFrontModelTest
+    {
+        [Fact]
+        public void StoreFronStoreNameShouldValidData()
+        {
+            StoreFront storeName = new StoreFront();
+            string validName = "Car parts shop 4";
+
+            storeName.StoreName = validName;
+
+            Assert.NotNull(storeName.StoreName);
+            Assert.Equal(validName, storeName.StoreName);
+        }
+    }
 }
