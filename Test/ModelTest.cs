7ab93f56adbcdb62c72f9798c2bd81d5fe3e8f28
Hyunsoo Jeon <hsprime85@gmail.com>
2022-02-28 11:46:46 -0800

    updated model test

--- Test/ModelTest.cs
+++ Test/ModelTest.cs
@@ -3,39 +3,6 @@ using Models;
 
 namespace Test
 {
-    public class ProductModelTest
-    {
-        [Fact]
-        public void PriceShouldValidData()
-        {
-            //arrange
-            Product price = new Product();
-            int validPrice = 100;
-
-            //act
-            price.Price = validPrice;
-
-            // assert
-            Assert.NotNull(price.Price);
-            Assert.Equal(validPrice, price.Price);
-
-        }
-
-        [Theory]
-        [InlineData(-9)]
-        [InlineData(-100)]
-        [InlineData(-500)]
-        public void PriceShouldFailSetInvalidData(int p_invalidPrice)
-        {
-            // arrange
-            Product price = new Product();
-
-            // act & assert
-            Assert.Throws<System.Exception>(
-                () => price.Price = p_invalidPrice
-            );
-        }
-    }
     public class CustomerModelTest
     {
         [Fact]
